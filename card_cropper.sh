6ebedc362ecd6764d9c76d100bcc39a768173370
klin1234@gmail.com <klin1234@gmail.com>
2017-08-25 16:35:56 -0400

    Added the cropping for energy, special energy, break

--- card_cropper.sh
+++ card_cropper.sh
@@ -19,3 +19,36 @@ for card in $files
 do
   convert $card -crop 633x387+966+408 "../cards/$card"
 done
+
+##############
+cd $home
+cd "raw_cards_energy"
+files=`ls`
+
+for card in $files
+do
+  convert $card -crop 633x387+962+584 "../cards/$card"
+done
+
+
+##############
+cd $home
+cd "raw_cards_special_energy"
+files=`ls`
+
+for card in $files
+do
+  convert $card -crop 685x419+941+443 "../cards/$card"
+  convert "../cards/$card" -resize 633x387 "../cards/$card"
+done
+
+##############
+cd $home
+cd "raw_cards_break"
+files=`ls`
+
+for card in $files
+do
+  convert $card -background white -virtual-pixel background +distort ScaleRotateTranslate +90 +repage "../cards/$card"
+  convert "../cards/$card" -crop 633x387+533+1070 "../cards/$card"
+done
