7129cc0651f9b5452aa29b180032041d9016f4b8
klin1234@gmail.com <klin1234@gmail.com>
2017-08-25 12:02:39 -0400

    Cropped some of the trainer cards too
    Some trainer cards are annoying since the older ones have non-rectangular
      borders...

--- card_cropper.sh
+++ card_cropper.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+home=`pwd`
+cd $home
 cd "raw_cards_pokemon"
 files=`ls`
-echo "$files"
-#echo `pwd`
 
 for card in $files
 do
   convert $card -crop 633x387+962+356 "../cards/$card"
+done
+
+##############
+cd $home
+cd "raw_cards_trainer"
+files=`ls`
+
+for card in $files
+do
+  convert $card -crop 633x387+966+408 "../cards/$card"
 done
