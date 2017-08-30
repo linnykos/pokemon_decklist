f371a32eba76b75de247064f5547570d27b103d4
klin1234@gmail.com <klin1234@gmail.com>
2017-08-30 09:59:37 +0800

    Fixed database creation to omit for NA rows

--- ptcgo/data-raw/create_database.R
+++ ptcgo/data-raw/create_database.R
@@ -4,6 +4,13 @@ file.remove("data-raw/db.sqlite")
 db <- DBI::dbConnect(RSQLite::SQLite(), dbname="data-raw/db.sqlite")
 
 dat <- read.csv("data-raw/database/card_db.csv")
+
+#remove rows with NA
+idx <- which(apply(dat, 1, function(x){
+  ifelse(any(is.na(x)), FALSE, TRUE)
+}))
+dat <- dat[idx,]
+
 DBI::dbWriteTable(conn = db, name = "card", value = dat, row.names = FALSE)
 
 #DBI::dbListTables(db)
