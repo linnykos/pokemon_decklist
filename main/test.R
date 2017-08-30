d7337a3c7e4076e49025974c7a8ab67ece55399c
klin1234@gmail.com <klin1234@gmail.com>
2017-08-31 03:51:19 +0800

    Some more testing

--- main/test.R
+++ main/test.R
@@ -1,6 +1,6 @@
 #download font
 
-deck_file = "../main/hidden_moon.txt"
-filename = "../main/test.png"
+deck_file = "../main/greninja_v2.txt"
+filename = "../main/greninja.png"
 
 decklist(deck_file, filename)
